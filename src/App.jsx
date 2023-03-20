c4d0ec39a42f6c922ae2c45b50ccf60899c3010a
Donny <doniwahyu14@gmail.com>
2023-03-20 22:01:14 +0700

    add tailwind styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Tech,
+  Navbar,
+  Works,
+  StarsCanvas,
+} from "./components";
+
 function App() {
-  return <div>3D Portfolio</div>;
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary bg-">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
