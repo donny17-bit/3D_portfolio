7aa75cb00356b8a82c939a18c29ca3c819dcc7e6
Donny <doniwahyu14@gmail.com>
2023-03-21 19:17:05 +0700

    add 3d object in the hero section

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import { Html, useProgress } from "@react-three/drei";
 
 const Loader = () => {
-  return (
-    <div>Loader</div>
-  )
-}
+  const { progress } = useProgress();
+  return <div>Loader</div>;
+};
 
-export default Loader
+export default Loader;
