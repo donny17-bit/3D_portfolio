01a7b675e576cd4533c1335a96ca869cb9512632
Donny <doniwahyu14@gmail.com>
2023-03-26 19:29:29 +0700

    add Experience section

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -11,13 +11,11 @@ const SectionWrapper = (Component, idName) =>
           &nbsp;
         </div>
         <motion.section
-          // variants={staggerContainer()}
           variants={staggerContainer()}
           initial="hidden"
           whileInView="show"
           viewport={{ once: true, amount: 0.25 }}
           className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
-          // id={idName}
         >
           <Component />
         </motion.section>
